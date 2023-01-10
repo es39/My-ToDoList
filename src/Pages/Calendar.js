5652524be02ac6100dd65600751723224e86ca3c
es39 <esj10025@gmail.com>
2023-01-10 15:56:01 +0900

    Calendar.js modify

---
+++ src/Pages/Calendar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import styled from "styled-components";
+
+export const Calendar = () => {
+    return (
+      <header>
+        This area is Header
+      </header>
+    );
+  }
+  
+export default Calendar;
