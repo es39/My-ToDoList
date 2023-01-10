8a646439c71a7d72eefb6d0848c4e40b280c22a7
es39 <esj10025@gmail.com>
2023-01-10 15:50:33 +0900

    Header.js modify

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import styled from "styled-components";
+
+export const Header = () => {
+    return (
+      <header>
+        This area is Header
+      </header>
+    );
+  }
+  
+export default Header;
