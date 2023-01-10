ca527bca0d2a5aa3601aa7ef4241dee31b5c9e53
es39 <esj10025@gmail.com>
2023-01-10 16:34:31 +0900

    Nav.js modify

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Nav = () => {
     return (
-      <header>
-        This is Nav
-      </header>
+      <main>
+        <FontAwesomeIcon icon="fa-solid fa-bars" />
+      </main>
     );
   }
   
