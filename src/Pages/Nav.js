6d20233edf737aae5972bce1cd8a6fc8502334b0
es39 <esj10025@gmail.com>
2023-01-10 15:54:32 +0900

    Nav.js modify

---
+++ src/Pages/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import styled from "styled-components";
+
+export const Nav = () => {
+    return (
+      <header>
+        This is Nav
+      </header>
+    );
+  }
+  
+export default Nav;
