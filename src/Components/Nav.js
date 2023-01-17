213a7c982319a4fd2eb6558d08e86c6789614909
es39 <esj10025@gmail.com>
2023-01-17 17:11:16 +0900

    Nav.js move and modify

---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import styled from "styled-components";
+
+export default Nav = () => {
+  return (
+    <nav>
+        
+    </nav>
+  );
+}
+  
