7192107e0c928899b94da57ac84a82e064b29b4c
es39 <esj10025@gmail.com>
2023-01-17 17:12:09 +0900

    Footer.js modify

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
+const FooterStyle= styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 export const Footer = () => {
     return (
-      <footer>
+      <FooterStyle>
         @ 2023 Sunny Day
-      </footer>
+      </FooterStyle>
     );
   }
   
