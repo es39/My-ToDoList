6b69f3ac16c812909ffacea89b0bc72beea42d8d
es39 <esj10025@gmail.com>
2023-01-17 17:11:37 +0900

    Main.js add

---
+++ src/Pages/Main.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import GlobalStyle from '../GlobalStyle'
+
+export default function Main() {
+  return (
+      <div className="Main">
+        <GlobalStyle />
+          <main>
+            <Link to='/todolist'><img src="todolistlogo.png" alt="투두리스트 로고"/></Link>
+          </main>
+      </div>
+  );
+}
