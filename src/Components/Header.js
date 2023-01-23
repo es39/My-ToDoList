d35bdb95fb84ada88f0e7b19747f11ec615f04e3
es39 <esj10025@gmail.com>
2023-01-23 23:04:03 +0900

    Modal add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useState } from "react"
 
+import Modal from './Modal'
 import Nav from './Nav'
 
 const HeaderStyle = styled.header`
@@ -18,6 +19,10 @@ const HeaderStyle = styled.header`
     box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.4);
     width: 100%;
 `
+const ModalBtn = styled.button`
+    border: none;
+    background-color: transparent;
+`
 const ImgWrapper = styled.div`
   width: 30px;
   height: 40px;
@@ -27,13 +32,19 @@ const ImgWrapper = styled.div`
   margin-left: 20px;
 `
 
-export const Header = ({ modalOpen, setModalOpen }) => {
+export const Header = () => {
+    const [modal, setModal] = useState(false)
+    const openModalHandler = () => {
+      setModal(!modal)
+    }
+
     return (
       <HeaderStyle>
         <ImgWrapper>
           <img src="logo2.png" alt="동물의 숲 나뭇잎 로고"/>
         </ImgWrapper>
-        <Nav />
+        <ModalBtn onClick={openModalHandler}><i class="fa-solid fa-bars"></i></ModalBtn>
+          {modal === true ? <Modal modal={modal} setModal={setModal} /> : null}
       </HeaderStyle>
     );
 }
