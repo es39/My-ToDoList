23ad172e9404d921fe89ef55f6846b35a74701e3
es39 <esj10025@gmail.com>
2023-02-02 20:57:10 +0900

    comment add

--- src/Components/Todolistitem.js
+++ src/Components/Todolistitem.js
@@ -2,9 +2,15 @@ export const Todolistitem = () => {
   
     return (
       <main>
-        
+
       </main>
     );
   }
   
 export default Todolistitem;
+
+/* 
+Todolistitem.js 구현 목표 체크리스트
+1. 아이콘 삽입 
+2. 아이콘 클릭 시 수정 및 삭제 가능
+*/
