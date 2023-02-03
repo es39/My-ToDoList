c091dd94d20529734b55de75b767ae5064e031dd
es39 <esj10025@gmail.com>
2023-02-03 21:53:40 +0900

    들여쓰기 수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,26 @@
 import React from "react";
 import styled from "styled-components";
-import { useState } from "react"
+import { useState } from "react";
 
-import Modal from './Modal'
-import Nav from './Nav'
+import Modal from "./Modal";
 
 const HeaderStyle = styled.header`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 80px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    border-radius: 10px;
-    box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.4);
-    width: 100%;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.4);
+`;
 const ModalBtn = styled.button`
-    border: none;
-    background-color: transparent;
-`
+  border: none;
+  background-color: transparent;
+`;
 const ImgWrapper = styled.div`
   width: 30px;
   height: 40px;
@@ -30,25 +28,27 @@ const ImgWrapper = styled.div`
   position: fixed;
   left: 0;
   margin-left: 20px;
-`
+`;
 
 export const Header = () => {
-    const [modal, setModal] = useState(false)
-    const openModalHandler = () => {
-      setModal(!modal)
-    }
+  const [modal, setModal] = useState(false);
+  const openModalHandler = () => {
+    setModal(!modal);
+  };
 
-    return (
-      <HeaderStyle>
-        <ImgWrapper>
-          <img src="logo2.png" alt="동물의 숲 나뭇잎 로고"/>
-        </ImgWrapper>
-        <ModalBtn onClick={openModalHandler}><i class="fa-solid fa-bars"></i></ModalBtn>
-          {modal === true ? <Modal modal={modal} setModal={setModal} /> : null}
-      </HeaderStyle>
-    );
-}
-  
+  return (
+    <HeaderStyle>
+      <ImgWrapper>
+        <img src="logo2.png" alt="동물의 숲 나뭇잎 로고" />
+      </ImgWrapper>
+      <ModalBtn onClick={openModalHandler}>
+        <i className="fa-solid fa-bars"></i>
+      </ModalBtn>
+      {modal === true ? <Modal modal={modal} setModal={setModal} /> : null}
+    </HeaderStyle>
+  );
+};
+
 export default Header;
 
 /* 
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,67 +1,69 @@
 import React from "react";
 import styled from "styled-components";
-import Nav from './Nav'
+import Nav from "./Nav";
 
-const ModalStyle = styled.div `
-    display: flex;
-    justify-content: center;
-    // align-items: center;
-    height: 100%;
-`
+const ModalStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+`;
 const ModalBtn = styled.button`
-    border: none;
-    background-color: transparent;
-`
+  border: none;
+  background-color: transparent;
+`;
 const ModalBack = styled.div`
-    background: rgba(0, 0, 0, 0.5); 
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    // flex-direction: row-reverse;
-    z-index: 999;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-`
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
 export const ModalView = styled.div.attrs((props) => ({
-    role: 'dialog',
-  }))`
-    background-color: white;
-    width: calc(100%/2);
-    height: 100vh;
-    color: black;
-    border-radius: 15px;
-    text-align: center;
-    position: fixed;
-    top: 0;
-    right: 0;
-    // bottom: 0;
-    z-index: 999;
-  `;
+  role: "dialog",
+}))`
+  background-color: white;
+  width: calc(100% / 2);
+  height: 100vh;
+  color: black;
+  border-radius: 15px;
+  text-align: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 999;
+`;
 
 export const Modal = ({ modal, setModal }) => {
-    const openModalHandler = () => {
-        setModal(!modal)
-    }
+  const openModalHandler = () => {
+    setModal(!modal);
+  };
 
-    return (
-        <ModalStyle>
-            <ModalBack onClick={openModalHandler}>
-                <ModalView onClick={(event) => {event.stopPropagation()}}>
-                    <ModalBtn onClick={openModalHandler}>x</ModalBtn>
-                        <Nav />
-                </ModalView>
-            </ModalBack>
-        </ModalStyle>
-    )
-}
+  return (
+    <ModalStyle>
+      <ModalBack onClick={openModalHandler}>
+        <ModalView
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          <ModalBtn onClick={openModalHandler}>x</ModalBtn>
+          <Nav />
+        </ModalView>
+      </ModalBack>
+    </ModalStyle>
+  );
+};
 
 export default Modal;
 
 /* 
+TODO:
   Modal.js 구현 목표 체크리스트
   1. 모달 배경 구현 *
   2. 모달창 구현 * 
